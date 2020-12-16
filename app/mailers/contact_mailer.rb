df06e976e8b15b51406e7aee1c10b6923f9e076e
Maximilian Glasmacher <maximilian@glasmacher.com>
2020-12-16 10:53:27 +0100

    commit

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,6 +3,6 @@ class ContactMailer < ApplicationMailer
 
   def general_message(contact)
     @contact = contact
-    mail( :to => "flori.friends.animalhope@gmail.com", :subject => "Kontaktanfrage #{@contact.dog.name if !@contact.dog.nil?}")
+    mail(:to => "flori.friends.animalhope@gmail.com", :subject => "Kontaktanfrage #{@contact.dog.name if !@contact.dog.nil?}")
   end
 end
